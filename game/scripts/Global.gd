7b5daefbbb3612abaebff95ec6ad3d8b6f333363
Elena FRISON <e_gavrilova_frison@zohomail.eu>
2024-10-06 12:15:06 +0200

    Màj des variables globales (+listes de départements et de profs)

--- game/scripts/Global.gd
+++ game/scripts/Global.gd
@@ -3,3 +3,5 @@ extends Node
 
 # Déclaration des variables globales ici
 var student_list: Array = [] # Liste d'étudiants
+var dept_list: Array = ["Chimie", "Génie civil", "Information-communication", "Informatique", "Techniques de commercialisation"] # Liste de départements
+var teacher_list: Array = [] # Liste d'enseignants
