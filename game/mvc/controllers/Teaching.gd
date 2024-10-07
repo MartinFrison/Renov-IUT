05f202773a7b204b1ebbb58628dd2791b59a66a5
Elena FRISON <e_gavrilova_frison@zohomail.eu>
2024-10-07 15:40:38 +0200

    Contrôleur des enseignants, Teaching.gd : opérations de base (ajout/suppression, création du corps enseignant, décompte par département, satisfaction par département

--- game/mvc/controllers/Teaching.gd
+++ game/mvc/controllers/Teaching.gd
@@ -9,19 +9,16 @@ func add_teacher(department: String) -> void:
 	Global.teacher_list.append(new_teacher)
 
 # Destructeur 1
-# Supprime un enseigant du tableau par ID.
+# Supprime un enseignant du tableau par ID.
 func rm_teacher_by_id(i: int) -> void:
-	if i < Global.student_list.size() and i > 0:
-		Global.student_list.remove_at(i)
+	if i < Global.teacher_list.size() and i >= 0:
+		Global.teacher_list.remove_at(i)
 		
 # Destructeur 2
 # Supprime un enseignant du tableau par département, en nombre spécifié.
 func rm_teacher_by_dept(dept: String, nb: int) -> void:
 	# Compter les enseignants du département concerné
-	var count: int = 0
-	for teacher in Global.teacher_list:
-		if teacher.get_dept() == dept:
-			count += 1
+	var count: int = compute_nb(dept)
 	
 	# Vérifier le nombre d'enseignants à faire partir
 	assert(nb <= count, "Tentative de suppression de {} enseignants sur {}.".format(str(nb), str(count)))
@@ -32,20 +29,57 @@ func rm_teacher_by_dept(dept: String, nb: int) -> void:
 		var teacher: Teacher = Global.teacher_list[i]
 		# Vérifiez si l'enseignant appartient au département spécifié
 		if teacher.get_dept() == dept:
-			while rm_count >= count:
-				Global.teacher_list.remove_at(i)
-				rm_count += 1
+			Global.teacher_list.remove_at(i)
+			rm_count += 1
+			if rm_count >= nb:
+				break;
+
+# Embauche tous les enseignants, par département (données empruntées sur Ernest)
+func populate() -> void:
+	# 21 enseignants en Chimie
+	for pers in range (21):
+		add_teacher("Chimie")
+		
+	# 24 enseignants en Génie civil
+	for pers in range (24):
+		add_teacher("Génie civil")
+		
+	# 18 enseignants en Info-Comm
+	for pers in range (18):
+		add_teacher("Information-communication")
+		
+	# 27 enseignants en Informatique
+	for pers in range (27):
+		add_teacher("Informatique")
+		
+	# 18 enseignants en Techniques de commercialisation
+	for pers in range (18):
+		add_teacher("Techniques de commercialisation")
 	
-# À implémenter ici : 
-# func populate() -> void
-# func compute_nb(dept: String) -> void:
-# func avg_mood(dept: String) -> float:
+# Renvoie le nombre d'enseignants pour tel département (passé en paramètre) 
+func compute_nb(dept: String) -> int:
+	var count: int = 0
+	for teacher in Global.teacher_list:
+		if teacher.dept == dept:
+			count += 1
+	return count
 
+# Calcule et renvoie la moyenne de la satisfaction des enseignants, par département
+func avg_mood(dept: String) -> float:
+	var sum: float = 0.0
+	for teacher in Global.teacher_list:
+		if teacher.dept == dept:
+			sum += teacher.mood
+	var nb: int = compute_nb(dept)
+	if nb == 0:
+		return 0.0
+	return sum / nb
 
-# Called when the node enters the scene tree for the first time.
+# Lors du premier appel
 func _ready() -> void:
-	pass # Replace with function body.
+	populate()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# ajouter la gestion des profs
 	pass
