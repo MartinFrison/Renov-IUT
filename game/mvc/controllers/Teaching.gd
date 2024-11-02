0b118b7a825e15c15388d82dce5c83183ad8d755
Elena FRISON <e_gavrilova_frison@zohomail.eu>
2024-11-02 22:23:48 +0100

    Teaching.gd : peuplement initial en fonction du département

--- game/mvc/controllers/Teaching.gd
+++ game/mvc/controllers/Teaching.gd
@@ -1,12 +1,13 @@
 class_name Teaching
 extends RefCounted
 
+const teachers_base_nb: Array = [21, 24, 18, 27, 18] # chiffres réels tirés du site officiel de l'IUT Robert Schuman
 
 # Fonction pour embaucher un professeur dans un département spécifique
 func hire_teachers(dept: String):
-	#vérifie si il y a moins de 20 profs dans le batiments
-	if Teacher.compute_nb_per_dept(dept) >= 20:
-		print("Deja max de prof dans ce batiments")
+	#vérifie si il y a moins de 30 profs dans le batiment
+	if Teacher.compute_nb_per_dept(dept) >= 30:
+		print("Deja max de prof dans ce batiment")
 		return
 
 	#vérifie si un prof est prêt à être recruté
@@ -26,10 +27,14 @@ func fire_teachers(dept: String):
 		return
 	
 	Teacher.rm_teachers_by_dept(dept,1)
-	print("Licenciement d'un professeurs pour le département %s" % dept)
+	print("Licenciement d'un professeur pour le département %s" % dept)
 
 
 
 # Fonction pour embaucher des enseignants au départ
 func populate():
-	print("Population initiale des professeur")
+	var nb_teachers = 0
+	for i in range(0,5): # par département
+		nb_teachers = teachers_base_nb[i]
+		for j in range (0, nb_teachers):
+			Teacher.add_teacher(Utils.dept_index_to_string(i), true) # Au départ, on n'embauchera que des profs titulaires
