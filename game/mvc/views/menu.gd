e4ac8985d7c794dd8a2b62a5b1d63222853551be
Chettati Yasmine <yasmine.chettati@etu.unistra.fr>
2024-11-22 10:48:59 +0100

    menu

---
+++ game/mvc/views/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+# Fonction appelée lorsque le bouton "Quitter le jeu" est pressé
+func _on_QuitterButton_pressed():
+	get_tree().quit()  # Ferme le jeu
+
+# Fonction appelée lorsque le bouton "Fermer le menu" est pressé
+func _on_FermerMenuButton_pressed():
+	hide()  # Cache le menu
+
+
+
+func _on_OuvrirPanelButton_pressed() -> void:
+	print("Ouvrir les options...")  # Remplacer par le code pour afficher les options plus tard
