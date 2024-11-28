31fdc92ff6e8dc36cc053e5e5c886041af4625f1
Martin Frison <martin.frison@unistra.fr>
2024-11-28 14:37:14 +0100

    Grossisement des batiments au passage de la souris

--- game/mvc/views/Node3D/camera_3d.gd
+++ game/mvc/views/Node3D/camera_3d.gd
@@ -1,5 +1,20 @@
 extends Camera3D
 
+#Taille par defaut
+var _default_scale = [0,0,0,0,0,0]
+var _building : Array[MeshInstance3D] = [null, null, null, null, null, null]
+var big = false
+
+func _ready() -> void:
+	_building[1] = get_tree().current_scene.get_node("Vue3D/IUT_V4/batiment_chimie") as MeshInstance3D
+	_building[2] = get_tree().current_scene.get_node("Vue3D/IUT_V4/batiment_genie_civil") as MeshInstance3D
+	_building[3] = get_tree().current_scene.get_node("Vue3D/IUT_V4/Batiment_Administration") as MeshInstance3D
+	_building[4] = get_tree().current_scene.get_node("Vue3D/IUT_V4/bat_info") as MeshInstance3D
+	_building[5] = get_tree().current_scene.get_node("Vue3D/IUT_V4/Batiment_infocom") as MeshInstance3D
+	for i in range (1,6):
+		_default_scale[i] = _building[i].scale
+
+
 func _input(event):
 	# Vérifiez si c'est un clic gauche de la souris
 	if event is InputEventMouseButton and event.pressed:
@@ -41,11 +56,19 @@ func handle_hover(event : InputEventMouseMotion):
 	var result = space_state.intersect_ray(query)
 	
 	if result:
-		print("Clique détecté sur : ", result.collider.name)
 		for i in range(1, 6):
 			if "colision" + str(i) == result.collider.name:
 				big_build(i)
+				return
+	if big == true:
+		big_build(-1)
 
-func big_build(i : int) -> void:
-	var iut = get_tree().current_scene.get_node("Vue3D/IUT_V4")
-	print(iut.name)
+
+func big_build(build : int) -> void:
+	for i in range(1,6):
+		_building[i].scale = _default_scale[i] * 1
+	if build > 0:
+		_building[build].scale = _default_scale[build] * 1.12
+		big = true
+	else:
+		big = false
