5893df5306c511d4ef6db8928e71672837266cad
Martin Frison <martin.frison@unistra.fr>
2024-11-28 15:24:15 +0100

    Ajout des control camera (trajectoire pas encore bien defini)

--- game/mvc/views/Node3D/camera_root.gd
+++ game/mvc/views/Node3D/camera_root.gd
@@ -1,15 +1,36 @@
 extends Node3D
 
 var _default_scale
-var _scale_coeff = 1
+var _rot_Y = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_default_scale = scale
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotate_object_local(Vector3(0, 1, 0), deg_to_rad(0.1))
-	scale = _default_scale * _scale_coeff
-	_scale_coeff += 0.0001
+	rotate_root(0.4)
+
+
+func curve_scale(degrees : float) -> float:
+	var radians = deg_to_rad(degrees)  # Convertit en radians
+	var result = sin(radians)  # Calcule le sinus
+	return result
+
+
+func rotate_root(coeff : float):
+	rotate_object_local(Vector3(0, 1, 0), deg_to_rad(coeff))
+	_rot_Y += coeff
+	if _rot_Y >= 360:
+		_rot_Y -= 360
+	if _rot_Y < 0:
+		_rot_Y += 360
+	scale = _default_scale * (1 + curve_scale(_rot_Y))
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		event = event as InputEventKey
+		if event.keycode == KEY_RIGHT:
+			rotate_root(-0.5)
+		elif event.keycode == KEY_LEFT:
+			rotate_root(0.5)
