41f6bbde818f370ed3c175fb23b86768337079e2
Martin Frison <martin.frison@unistra.fr>
2024-11-28 16:18:11 +0100

    correction de l'orbit de la camera

--- game/mvc/views/Node3D/camera_root.gd
+++ game/mvc/views/Node3D/camera_root.gd
@@ -7,14 +7,16 @@ var _rot_Y = 0
 func _ready() -> void:
 	_default_scale = scale
 
+
+
 func _process(delta: float) -> void:
-	rotate_root(0.4)
+	pass
 
 
 func curve_scale(degrees : float) -> float:
 	var radians = deg_to_rad(degrees)  # Convertit en radians
 	var result = sin(radians)  # Calcule le sinus
-	return result
+	return abs(result)
 
 
 func rotate_root(coeff : float):
@@ -24,13 +26,14 @@ func rotate_root(coeff : float):
 		_rot_Y -= 360
 	if _rot_Y < 0:
 		_rot_Y += 360
-	scale = _default_scale * (1 + curve_scale(_rot_Y))
+	scale = _default_scale * (1 + (curve_scale(_rot_Y)/2))
+
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
 		event = event as InputEventKey
 		if event.keycode == KEY_RIGHT:
-			rotate_root(-0.5)
-		elif event.keycode == KEY_LEFT:
 			rotate_root(0.5)
+		elif event.keycode == KEY_LEFT:
+			rotate_root(-0.5)
