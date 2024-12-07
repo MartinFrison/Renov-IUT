f5a722b53e253baa4e482c736737846b20babc14
Martin Frison <martin.frison@unistra.fr>
2024-12-07 13:55:24 +0100

    visibilité des bonhomme en été + leur distance de marche

--- game/mvc/views/Node3D/character/bonhomme.gd
+++ game/mvc/views/Node3D/character/bonhomme.gd
@@ -1,13 +1,18 @@
 class_name Bonhomme
 extends Node3D
 
+@export var distance: float = 300
+
 @onready var animation_player = $AnimationPlayer
 
-
+var visible_summer : bool
 
 func _ready() -> void:
-	var x = Utils.randint_in_range(0,190)
+	
+	var x = Utils.randint_in_range(0,distance*0.7)
 	translate(Vector3(0, 0, 0.1*x))
+	
+	visible_summer = (Utils.randint_in_range(0,100)<20)
 	
 	await get_tree().create_timer(Utils.randfloat_in_range(0.1,0.3)).timeout
 	
@@ -15,7 +20,7 @@ func _ready() -> void:
 		translate(Vector3(0, 0, 0.1))
 		await get_tree().create_timer(0.02).timeout
 		x += 1
-		if x>300:
+		if x>distance:
 			x = 0
 			rotate_object_local(Vector3(0, 1, 0), deg_to_rad(180))
 
@@ -23,9 +28,13 @@ func _ready() -> void:
 func content():
 	animation_player.play("Armature|marche_content")
 	animation_player.get_animation("Armature|marche_content").loop = true
-	
+	setVisible()
 	
 func facher():
 	animation_player.play("Armature|marche_facher")
 	animation_player.get_animation("Armature|marche_facher").loop = true
-	
+	setVisible()
+
+
+func setVisible():
+	visible = visible_summer or GlobalData._month<6 or GlobalData._month>8
