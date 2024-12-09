4a1ef7c06dfa827e2c0bbb862b04a7be575e26a5
Martin Frison <martin.frison@unistra.fr>
2024-12-10 00:45:16 +0100

    Affichage du bilan sur l'elitisme dans l'écran de fin

--- game/mvc/views/Node2D/FinJeu/FinElitismeScript.gd
+++ game/mvc/views/Node2D/FinJeu/FinElitismeScript.gd
@@ -10,8 +10,15 @@ func _process(delta: float) -> void:
 	pass
 
 
+# On affiche le bilan de la partie au joueur
 func show_result() -> void:
+	# Sur le nombre de d'étudiant ayant rejoins une école d'ingénieur
 	var node = get_node("Message")
-	node.text = "Sous votre manda, x étudiant on pu intégrer une école d'ingénieur
-	à l'issu de leur formation"
+	node.text = "Sous votre manda, %s étudiant on pu intégrer une école 
+	d'ingénieur à l'issu de leur formation" % [Student.get_engineering()]
 	
+	# Sur le nombre de diplomé
+	node = get_node("Message2")
+	node.text = "D'autre part, %s étudiant on pu obtenir 
+	leur diplome soit %s par an.
+	" % [Student.get_graduate(), round(Student.get_graduate()/5)]
