960a48d4bb4a82023d47d529dce3a451412da173
Martin Frison <martin.frison@unistra.fr>
2024-12-11 20:09:47 +0100

    fonction d'affichage et masquage de la bulle avec taille et position en parametre

--- game/mvc/controllers/Timeline/Tutorial.gd
+++ game/mvc/controllers/Timeline/Tutorial.gd
@@ -3,6 +3,7 @@ extends Node
 
 
 var _tutorial : bool # Défini si le tutoriel est activé ou non
+var _tuto_buble : BulleTutorial
 
 
 func _init(tuto : bool) -> void:
@@ -13,8 +14,9 @@ func _init(tuto : bool) -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var scene = load("res://mvc/views/Node2D/Bulle/BulleTuto/BulleTutorial.tscn")
-	var bulle = scene.instantiate()
-	RenovIUTApp.app.add_child(bulle)
+	_tuto_buble = scene.instantiate()
+	RenovIUTApp.app.add_child(_tuto_buble)
+	_tuto_buble.hide()
 
 
 
@@ -27,7 +29,8 @@ func tuto_start() -> void:
 	if _tutorial:
 		var msg
 		
+		_tuto_buble.show_buble(250,100,200,200)
 		msg = "Bienvenu dans Renov'IUT un jeu de gestion ou vous incarnez "
 		msg += "le directeur de l'IUT Robert Schuman"
 		await BulleGestion.send_message(msg, false)
-		
+		_tuto_buble.hide()
--- game/mvc/views/Node2D/Bulle/BulleTuto/BulleTutorial.gd
+++ game/mvc/views/Node2D/Bulle/BulleTuto/BulleTutorial.gd
@@ -1,11 +1,28 @@
+class_name BulleTutorial
 extends Node2D
 
+var buble : Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	buble = get_node("Panel")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func hide_buble():
+	visible = false
+
+
+func show_buble(sx:int, sy:int, px:int, py:int):
+	visible = true
+	buble.size.x = sx
+	buble.size.y = sy
+	buble.position.x = px
+	buble.position.y = py
+	
+	
+	
