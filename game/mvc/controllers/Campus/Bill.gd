22b0bf88023ef243614402168f5df65c2ed685d6
Martin Frison <martin.frison@unistra.fr>
2024-12-18 14:23:29 +0100

    trimestrialisation des factures

--- game/mvc/controllers/Campus/Bill.gd
+++ game/mvc/controllers/Campus/Bill.gd
@@ -18,8 +18,8 @@ func reset_bill() -> void:
 	nb_pay_worker = 0
 	pay_heating = [0,0,0,0,0,0]
 
-# Rajoute à la facture les sommes pour n jours
-func add_daily_expense(day : int) -> void:
+# Rajoute à la facture les sommes pour un trimestre
+func add_daily_expense() -> void:
 	var b
 	var code
 	var sumWorker = 0
@@ -28,15 +28,16 @@ func add_daily_expense(day : int) -> void:
 		b = Building.get_building(code)
 		
 		sumWorker += b.get_ouvriers()
-		pay_teacher[i+1] += (b.get_pay_teacher() * Teacher.compute_nb_per_dept(code))*day/30	
-		pay_worker[i+1] += (b.get_ouvriers() * GlobalData.get_pay_worker())*day/30
+		pay_teacher[i+1] += (b.get_pay_teacher() * Teacher.compute_nb_per_dept(code))*3	
+		pay_worker[i+1] += (b.get_ouvriers() * GlobalData.get_pay_worker())*3
 		
-		# Facture de chauffage (cout en fonction de l'isolation)
+		# Facture de chauffage 
+		# cout en fonction de l'état des batiments et de leur taille
 		if b.is_heating():
-			var heat_cost = Building.MonthlySquareMetersHeatingCost * b.get_surface() / 30
-			# heat_cost = heat_cost*(1 + b.get_isolation()/25) # Entre 20% et 100% du prix selon l'isolation
-			# Ça bogue : soit supprimer l'isolation partout, soit, au contraire, l'implémenter.
-			pay_heating[i+1] += heat_cost * day
+			var heat_cost = Building.MonthlySquareMetersHeatingCost * b.get_surface() * 3
+			heat_cost = heat_cost*(1 + b.get_inventory()/50) # Prix multiplier jusqu'a X2 selon
+			# l'état du batiment
+			pay_heating[i+1] += heat_cost
 	
 	nb_pay_teacher = max(nb_pay_teacher, Teacher.compute_nb())
 	nb_pay_worker = max(nb_pay_worker, sumWorker)
